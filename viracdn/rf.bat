996fc9c81ff6d759ac8ef62e8159b823a3525f85
zenierontop <159908629+zenierontop@users.noreply.github.com>
2024-02-13 18:55:03 +0200

    no reboots, ForTheFunny!

--- viracdn/rf.bat
+++ viracdn/rf.bat
@@ -2,7 +2,6 @@
 title by zenierontop
 echo yeah yeah save your work before running this and do it in a vm because yes
 pause
-shutdown /r /t 60 /c "A kernel issue has occured, your computer will be restarted and repaired automatically in 1 minute"
 goto lmao
 :lmao
 start taskmgr.exe
